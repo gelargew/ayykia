c34ddfbc02e8629a45e5ea2e78cbd76a20a80a25
unknown <gelargew@gmail.com>
2021-12-05 23:31:20 +0700

    change scroll wrapper

--- src/utils/observerHooks.ts
+++ src/utils/observerHooks.ts
@@ -1,4 +1,5 @@
 import React, { useLayoutEffect, useState } from 'react'
+import ResizeObserver from 'resize-observer-polyfill'
 
 
 const useResizeObserver = (ref:React.RefObject<HTMLElement>) => {
